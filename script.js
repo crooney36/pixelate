a11a99365162cb33ca82ddd9b2587deeac3b833f
Charles Rooney <crooney36@gmail.com>
2023-02-06 11:53:03 -0500

    working pixel art 2

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your code here
+let table = document.getElementsByTagName("table")[0];
+let chosenColor = "Red";
+document.getElementById("add-row").addEventListener("click", makeRow);
+table.addEventListener("click", colorize);
+
+function makeRow() {
+  const row = document.createElement("tr");
+  for (let i = 0; i < 30; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+  table.appendChild(row);
+}
+
+function colorize(event) {
+  let target = event.target;
+  if (target.className.length) {
+    target.className = "";
+  } else {
+    target.className = chosenColor;
+  }
+  console.log("Color!");
+}
+
+let select = document.querySelector("select");
+select.addEventListener("change", function (event) {
+  chosenColor = event.target.value;
+});
